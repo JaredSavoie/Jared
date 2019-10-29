5fb8b41a61e7c2d8a9d9f6cdd9d69ce04f654bb7
JaredSavoie <knuckleheaddavinci@gmail.com>
2019-10-29 10:40:16 -0500

    Ping batch file
    simple may not be needed but still made it

---
+++ ping.bat
@@ -0,0 +1,12 @@
+@echo off
+mode con: cols=53 lines=4
+set /P IPADDRESS="IP or Host Name: "
+Title ping %IPADDRESS%
+set INTERVAL=1
+:PINGINTERVAL
+ping %IPADDRESS% -n 1 | find "TTL="
+if errorlevel 1 (set itscolour=40 & echo FAIL!) ELSE (set itscolour=20)
+color %itscolour%
+timeout %INTERVAL%
+cls
+GOTO PINGINTERVAL
