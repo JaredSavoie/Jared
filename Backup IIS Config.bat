2f7247c82c23d489a90c46a660105d8fdd7a17e3
JaredSavoie <knuckleheaddavinci@gmail.com>
2019-11-06 13:17:58 -0600

    Update Backup IIS Config.bat

--- Backup IIS Config.bat
+++ Backup IIS Config.bat
@@ -1,16 +1,21 @@
 @echo off
 mode con: cols=90 lines=15
+:loop
 set /p BN= Backup Name:
 
 cd C:\Windows\System32\inetsrv
 
 appcmd add backup %BN%
 
-REM @echo Backup %BN% created
+if errorlevel 1 (
+color 40
+goto loop) ELSE ( 
+color 20)
+
 
 cd C:\Windows\System32\inetsrv\backup
 start .
 
-Pause
+rem Pause
 
 exit
