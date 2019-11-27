8815a8d6cd272b56733e5886eeb230bfff8f702f
JaredSavoie <knuckleheaddavinci@gmail.com>
2019-11-27 08:17:51 -0600

    Update Restore IIS Config.bat

--- Restore IIS Config.bat
+++ Restore IIS Config.bat
@@ -1,4 +1,5 @@
 rem This file needs to be ranas administrator
+rem for more information on the commands look here https://sid-500.com/2017/08/08/windows-server-2016-evaluation-how-to-extend-the-trial-period/
 @echo off
 mode con: cols=90 lines=15
 cd C:\Windows\System32\inetsrv
