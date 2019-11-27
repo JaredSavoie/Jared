062d89d14df126f024618c4da14f5587124a9c92
JaredSavoie <knuckleheaddavinci@gmail.com>
2019-11-27 08:27:31 -0600

    Update Extend Evaluation.ps1

--- Extend Evaluation.ps1
+++ Extend Evaluation.ps1
@@ -1,3 +1,10 @@
+# for more information on the commands look here https://sid-500.com/2017/08/08/windows-server-2016-evaluation-how-to-extend-the-trial-period/
+#
 slmgr -dlv
+pause
+#
 slmgr -rearm
+write-output "system will now restart"
+pause
+#
 Restart-Computer
