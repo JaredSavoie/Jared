8a106a3de5a07ba0dc47385877790131d40d0659
JaredSavoie <knuckleheaddavinci@gmail.com>
2019-11-27 09:41:25 -0600

    Update Rename SwDistrabution and Catroot folder.bat

--- Rename SwDistrabution and Catroot folder.bat
+++ Rename SwDistrabution and Catroot folder.bat
@@ -1,25 +1,47 @@
 @echo off
-echo "this script must be ranas administrator"
+echo "This script must be ran as Administrator"
 pause
 
+rem stop services
+::-------------------------------------------------------------------------------
+
 net stop wuauserv
-
 net stop cryptSvc
-
 net stop bits
-
 net stop msiserver
 
-ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+::-------------------------------------------------------------------------------
 
-ren C:\Windows\System32\catroot2 Catroot2.old
+rem rename SoftwareDistribution folder and delete the SoftwareDistribution.old
+::-------------------------------------------------------------------------------
+
+ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
+cd C:\Windows\SoftwareDistribution.old
+DEL /F/Q/S *.* > NUL
+cd..
+start.
+RMDIR /Q/S SoftwareDistribution.old
+
+::-------------------------------------------------------------------------------
+
+rem rename CatRoot2 folder and delete the CatRoot2.old
+::-------------------------------------------------------------------------------
+
+ren C:\Windows\System32\CatRoot2 CatRoot2.old
+cd C:\Windows\System32\CatRoot2.old
+DEL /F/Q/S *.* > NUL
+cd..
+start.
+RMDIR /Q/S CatRoot2.old
+
+::-------------------------------------------------------------------------------
+
+rem start services
+::-------------------------------------------------------------------------------
 
 net start wuauserv
-
 net start cryptSvc
-
 net start bits
-
 net start msiserver
 
-exit
+::-------------------------------------------------------------------------------
