9deebfbf50b364d7a5251bb3a6210957c1847dc8
JaredSavoie <knuckleheaddavinci@gmail.com>
2019-11-27 08:31:25 -0600

    Update Extend Evaluation.ps1

--- Extend Evaluation.ps1
+++ Extend Evaluation.ps1
@@ -1,6 +1,7 @@
 # for more information on the commands look here https://sid-500.com/2017/08/08/windows-server-2016-evaluation-how-to-extend-the-trial-period/
 #
 slmgr -dlv
+write-output "This script will reboot your Server. To proceed with the script to extend time press any key. if not press CTL + C"
 pause
 #
 slmgr -rearm
